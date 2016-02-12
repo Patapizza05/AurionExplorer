4762fbcb472d114b0fdf8c08b2e9a811d75d6ec1
Patapizza05 <c.duployez@gmail.com>
2016-02-12 13:46:33 +0100

    First fragment

---
+++ app/src/main/java/fr/clementduployez/aurionexplorer/HamburgerMenuManager.java
@@ -0,0 +1,99 @@
+package fr.clementduployez.aurionexplorer;
+
+import android.view.View;
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+
+import java.util.ArrayList;
+
+/**
+ * Created by cdupl on 2/12/2016.
+ */
+public class HamburgerMenuManager {
+
+    private final MainActivity activity;
+    private final AccountHeader header;
+    private final Drawer drawer;
+
+    private String name = "Clément Duployez";
+    private String id = "p58095";
+
+    private String[] titles = {"Mes Notes", "Mes Absences", "Mon Planning"};
+    private ArrayList<PrimaryDrawerItem> items = new ArrayList<>(3);
+
+    public HamburgerMenuManager(MainActivity activity) {
+        this.activity = activity;
+        initDrawerItems();
+        this.header = initAccountHeader();
+        this.drawer = initDrawer(this.header);
+    }
+
+    private void initDrawerItems() {
+        int size = titles.length;
+        for (int i = 0; i < size; i++) {
+            items.add(new PrimaryDrawerItem().withName(titles[i]));
+        }
+    }
+
+    private AccountHeader initAccountHeader() {
+        // Create the AccountHeader
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this.activity)
+                        //.withHeaderBackground(R.drawable.header)
+                .addProfiles(
+                        new ProfileDrawerItem()
+                                .withName(name)
+                                .withEmail(id)
+                                .withIcon(R.drawable.user)
+                )
+                .withSelectionListEnabledForSingleProfile(false)
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return false;
+                    }
+                })
+                .build();
+
+        headerResult.setBackgroundRes(R.color.colorPrimary);
+        return headerResult;
+    }
+
+    private Drawer initDrawer(AccountHeader header) {
+        //create the drawer and remember the `Drawer` result object
+        Drawer result = new DrawerBuilder()
+                .withActivity(this.activity)
+                .withToolbar(activity.getToolbar())
+                .withTranslucentStatusBar(false)
+                .withActionBarDrawerToggle(true)
+                .withActionBarDrawerToggleAnimated(true)
+                .withAccountHeader(header)
+                .withCloseOnClick(true)
+                .addDrawerItems(items.toArray(new PrimaryDrawerItem[items.size()]))
+                .withSelectedItemByPosition(0)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int i, IDrawerItem item) {
+                        drawer.closeDrawer();
+                        //Do something
+                        return true;
+                    }
+                })
+                .build();
+
+        result.getActionBarDrawerToggle().setDrawerIndicatorEnabled(true);
+
+        return result;
+    }
+
+    public String getSelectedItemTitle() {
+        return this.titles[this.drawer.getCurrentSelectedPosition()];
+    }
+}
--- app/src/main/java/fr/clementduployez/aurionexplorer/MainActivity.java
+++ app/src/main/java/fr/clementduployez/aurionexplorer/MainActivity.java
@@ -1,86 +1,36 @@
 package fr.clementduployez.aurionexplorer;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-
-import com.mikepenz.materialdrawer.AccountHeader;
-import com.mikepenz.materialdrawer.AccountHeaderBuilder;
-import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.DividerDrawerItem;
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import android.widget.FrameLayout;
 
 //Drawer Builder : http://android-arsenal.com/details/1/1526
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private HamburgerMenuManager hamburgerMenuManager;
+    private FrameLayout container;
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         toolbar = (Toolbar)findViewById(R.id.toolbar);
+        container = (FrameLayout)findViewById(R.id.frame_container);
 
-       this.setSupportActionBar(toolbar);
+        this.setSupportActionBar(toolbar);
+        this.hamburgerMenuManager = new HamburgerMenuManager(this);
 
-        //if you want to update the items at a later time it is recommended to keep it in a variable
-        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withName("Mes Notes");
-        PrimaryDrawerItem item2 = new PrimaryDrawerItem().withName("Mon Planning");
-        PrimaryDrawerItem item3 = new PrimaryDrawerItem().withName("Mes Absences");
-
-        // Create the AccountHeader
-        AccountHeader headerResult = new AccountHeaderBuilder()
-                .withActivity(this)
-                //.withHeaderBackground(R.drawable.header)
-                .addProfiles(
-                        new ProfileDrawerItem().withName("Clément Duployez").withEmail("c.duployez@gmail.com")//.withIcon(getResources().getDrawable(R.drawable.profile))
-                )
-                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
-                    @Override
-                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
-                        return false;
-                    }
-                })
-                .build();
-
-        //create the drawer and remember the `Drawer` result object
-        Drawer result = new DrawerBuilder()
-                .withActivity(this)
-                .withToolbar(toolbar)
-                .withTranslucentStatusBar(false)
-                .withActionBarDrawerToggle(true)
-                .withActionBarDrawerToggleAnimated(true)
-                .withAccountHeader(headerResult)
-                .addDrawerItems(
-                        item1,
-                        //new DividerDrawerItem(),
-                        item2,
-                        item3
-                )
-                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                    @Override
-                    public boolean onItemClick(View view, int i, IDrawerItem iDrawerItem) {
-                        //Do something
-                        return true;
-                    }
-                })
-                .build();
-
-        //result.getActionBarDrawerToggle().setDrawerIndicatorEnabled(true);
-        //getActionBar().setDisplayHomeAsUpEnabled(true);
-        //getActionBar().setHomeButtonEnabled(false);
-
+        openFragment(MarksFragment.newInstance());
     }
 
     @Override
@@ -104,4 +54,23 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public Toolbar getToolbar() {
+        return toolbar;
+    }
+
+    private void openFragment(Fragment fragment) {
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        if (fragment != null) {
+            if (this.currentFragment != null) {
+                transaction.remove(this.currentFragment);
+            }
+            transaction.add(R.id.frame_container, fragment);
+            this.currentFragment = fragment;
+            transaction.commit();
+        }
+    }
+
+
 }
