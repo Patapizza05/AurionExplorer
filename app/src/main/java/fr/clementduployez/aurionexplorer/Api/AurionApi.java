679770f77282e2bab7ac6fdbfc574d98deba4ce6
clement <c.duployez@gmail.com>
2016-11-21 22:40:54 +0100

    Planning from API

--- app/src/main/java/fr/clementduployez/aurionexplorer/Api/AurionApi.java
+++ app/src/main/java/fr/clementduployez/aurionexplorer/Api/AurionApi.java
@@ -4,12 +4,20 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import fr.clementduployez.aurionexplorer.Api.Annotations.AurionAnnotations;
+import fr.clementduployez.aurionexplorer.Api.Responses.EmptyPlanningResponse;
 import fr.clementduployez.aurionexplorer.Api.Responses.IndexResponse;
+import fr.clementduployez.aurionexplorer.Api.Responses.LoginFormResponse;
+import fr.clementduployez.aurionexplorer.Api.Responses.LoginResponse;
+import fr.clementduployez.aurionexplorer.Api.Responses.PlanningResponse;
 import fr.clementduployez.aurionexplorer.Informer;
 import fr.clementduployez.aurionexplorer.Utils.AurionCookies;
 import fr.clementduployez.aurionexplorer.Utils.Settings;
+import fr.clementduployez.aurionexplorer.Utils.UserData;
 
 /**
  * Created by cdupl on 11/21/2016.
@@ -17,14 +25,174 @@ import fr.clementduployez.aurionexplorer.Utils.Settings;
 
 public class AurionApi implements IAurionApi {
 
+    private static AurionApi instance = new AurionApi();
+
+    public static AurionApi getInstance() {
+        return instance;
+    }
+
+    @Override
+    public IndexResponse index() {
+        return index(true);
+    }
+
+    private IndexResponse index(boolean isFirstTry) {
+        Informer.inform(Messages.INDEX_START);
+        AurionAnnotations annotations = AurionAnnotations.getInstance("index", new Class[] {});
+        Connection.Response result = jsoupConnect(annotations);
+
+        if (result != null && result.statusCode() == 200) {
+            AurionCookies.addAll(result.cookies());
+
+            if (!isLoggedIn(result)) {
+                LoginFormResponse loginFormResponse = new LoginFormResponse(result);
+                if (isFirstTry) {
+                    relogin(loginFormResponse);
+                    return index(false);
+                }
+            }
+            return new IndexResponse(result);
+        }
+        else {
+            Informer.inform(Messages.INDEX_ERROR);
+        }
+        return null;
+    }
+
+    @Override
+    public LoginFormResponse loginForm() {
+
+        AurionAnnotations annotations = AurionAnnotations.getInstance("loginForm", new Class[] {});
+        Connection.Response result = jsoupConnect(annotations);
+
+        if (result != null && result.statusCode() == 200) {
+            AurionCookies.addAll(result.cookies());
+            return new LoginFormResponse(result);
+        }
+        return null;
+    }
+
+    @Override
+    public LoginResponse relogin(LoginFormResponse loginFormResponse) {
+        return login(loginFormResponse, UserData.getUsername(), UserData.getPassword());
+    }
+
+    @Override
+    public LoginResponse login(String username, String password) {
+        return login(loginForm(), username, password);
+    }
+
+    @Override
+    public LoginResponse login(LoginFormResponse loginFormResponse, String username, String password) {
+        AurionAnnotations annotations = AurionAnnotations.getInstance("login", new Class[] {LoginFormResponse.class, String.class, String.class });
+
+        Map<String, String> data = loginFormResponse.getHiddenInputData();
+        data.put(LoginFormResponse.USERNAME_INPUT_KEY, username);
+        data.put(LoginFormResponse.PASSWORD_INPUT_KEY, password);
+        data.put(LoginFormResponse.REDIRECT_KEY, Settings.Api.LOGIN_REDIRECT_URL);
+
+        Connection.Response result = jsoupConnect(annotations, data);
+
+        if (result != null && result.statusCode() == 200) {
+            AurionCookies.addAll(result.cookies()); //Keep user logged in
+            return new LoginResponse(result);
+        }
+
+        //In case of error, clear cookies to make sure user is disconnected
+        AurionCookies.clear();
+        return null;
+    }
+
+    @Override
+    public EmptyPlanningResponse planning() {
+        AurionAnnotations annotations = AurionAnnotations.getInstance("planning", new Class[] {});
+
+        IndexResponse indexResponse = index();
+
+        Informer.inform("Connection à la page \"Planning\" en cours");
+
+        Map<String, String> data = new HashMap<String, String>();
+        data.putAll(indexResponse.getHiddenInputData());
+        try {
+            data.putAll(PlanningResponse.prepareRequest(indexResponse, annotations.getTitle()));
+        }
+        catch(IOException ex) {
+            //Can't parse document
+            return null;
+        }
+
+        Connection.Response result = jsoupConnect(annotations, data);
+        if (result != null && result.statusCode() == 200) {
+            AurionCookies.addAll(result.cookies());
+
+            return new EmptyPlanningResponse(result);
+        }
+
+        return null;
+    }
+
+    @Override
+    public PlanningResponse planning(Date beginDate, Date endDate) {
+        AurionAnnotations annotations = AurionAnnotations.getInstance("planning", new Class[] { Date.class, Date.class });
+
+        EmptyPlanningResponse emptyPlanningResponse = planning(); //homePage --> planning
+
+        Map<String, String> data = PlanningResponse.prepareRequest(beginDate, endDate);
+        data.putAll(emptyPlanningResponse.getHiddenInputData());
+
+        Connection.Response result = jsoupConnect(annotations, data);
+        if (result != null && result.statusCode() == 200) {
+            AurionCookies.addAll(result.cookies());
+            try {
+                return new PlanningResponse(result);
+            } catch (IOException e) {
+                //
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void grades(int page) {
+
+    }
+
+    @Override
+    public void conferences(int page) {
+
+    }
+
+    @Override
+    public void birthdays() {
+
+    }
+
+    @Override
+    public void staffForm() {
+
+    }
+
+    @Override
+    public void staff(String status, String dataNom, String dataPrenom, String dataCode) {
+
+    }
+
     private Connection.Response jsoupConnect(AurionAnnotations annotations) {
+        return jsoupConnect(annotations, null);
+    }
+
+    private Connection.Response jsoupConnect(AurionAnnotations annotations, Map<String, String> data) {
         try {
             Connection connection = Jsoup.connect(annotations.getUrl())
-                    .timeout(Settings.CONNECTION_TIMEOUT)
-                    .userAgent(Settings.USER_AGENT)
-                    .followRedirects(true)
-                    .header("Content-Type", annotations.getContentType())
+                    .header(Settings.Api.CONTENT_TYPE_KEY, annotations.getContentType())
+                    .timeout(Settings.Api.CONNECTION_TIMEOUT)
+                    .userAgent(Settings.Api.USER_AGENT)
+                    .method(annotations.getHttpMethod())
                     .cookies(AurionCookies.get());
+
+            if (data != null) {
+                connection = connection.data(data);
+            }
 
             String referrer = annotations.getReferrer();
             if (referrer != null) {
@@ -41,72 +209,14 @@ public class AurionApi implements IAurionApi {
     }
 
     private boolean isLoggedIn(Connection.Response response) {
-        return !response.url().toString().startsWith("https://cas.isen.fr/login");
-    }
-
-    @Override
-    public IndexResponse Index() {
-        Informer.inform(Messages.INDEX_START);
-        AurionAnnotations annotations = AurionAnnotations.getInstance(this, "Index");
-        Connection.Response result = jsoupConnect(annotations);
-        if (result != null) {
-            AurionCookies.addAll(result.cookies());
-
-            if (!isLoggedIn(result)) {
-                //Exception
-                return null;
-            }
-            return new IndexResponse(result);
-        }
-        else {
-            Informer.inform(Messages.INDEX_ERROR);
-        }
-        return null;
-    }
-
-    @Override
-    public void LoginForm() {
-
-    }
-
-    @Override
-    public void Login(String username, String password) {
-
-    }
-
-    @Override
-    public void Planning() {
-
-    }
-
-    @Override
-    public void Grades(int page) {
-
-    }
-
-    @Override
-    public void Conferences(int page) {
-
-    }
-
-    @Override
-    public void Birthdays() {
-
-    }
-
-    @Override
-    public void StaffForm() {
-
-    }
-
-    @Override
-    public void Staff(String status, String dataNom, String dataPrenom, String dataCode) {
-
+        return !response.url().toString().startsWith(Settings.Api.LOGIN_URL);
     }
 
     private class Messages {
-        public static final String INDEX_START = "Chargement de la page d'accueil en cours";
-        public static final String INDEX_ERROR = "Erreur pendant le chargement de la page d'accueil";
+        static final String INDEX_START = "Chargement de la page d'accueil en cours";
+        static final String INDEX_ERROR = "Erreur pendant le chargement de la page d'accueil";
+
+        static final String LOGIN_START = "Identification en cours";
 
     }
 }
