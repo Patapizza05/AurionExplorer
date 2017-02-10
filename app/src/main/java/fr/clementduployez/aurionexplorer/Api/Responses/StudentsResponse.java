5d635ff22f0135f112174b63c9c2e3e57a1077fd
cdupl <c.duployez@gmail.com>
2017-02-10 09:59:14 +0100

    student directory parser

--- app/src/main/java/fr/clementduployez/aurionexplorer/Api/Responses/StudentsResponse.java
+++ app/src/main/java/fr/clementduployez/aurionexplorer/Api/Responses/StudentsResponse.java
@@ -8,7 +8,6 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 import fr.clementduployez.aurionexplorer.Model.StudentInfo;
 import fr.clementduployez.aurionexplorer.Settings.Settings;
@@ -26,23 +25,87 @@ public class StudentsResponse extends AbstractResponse {
         this.data = parseStudents(getDocument());
     }
 
-    public List<StudentInfo> parseStudents(Document document) {
+    private List<StudentInfo> parseStudents(Document document) {
 
         List<StudentInfo> students = new LinkedList<>();
 
         Elements containers = document.getElementsByClass("span2");
 
         for (Element studentContainer: containers) {
-            String imageUrl = studentContainer.getElementsByClass("img-polaroid").get(0).attr("src");
-            if (imageUrl != null && imageUrl.startsWith("/")) {
-                imageUrl = Settings.Api.CAS_URL + imageUrl;
+            String imageUrl = parseImageUrl(studentContainer);
+            String name = parseName(studentContainer);
+
+            String birthday = null;
+            String email = null;
+            String promo = null;
+            String specialization = null;
+
+            for(Element p : studentContainer.getElementsByTag("p")) {
+                if (p.ownText().startsWith("Date")) //de naissance
+                {
+                    birthday = parseDetail(p);
+                }
+                else if (p.ownText().startsWith("Majeure")) {
+                    specialization = parseDetail(p);
+                }
+                else if (p.ownText().startsWith("Courriel")) {
+                    email = parseEmail(p);
+                }
+                else if (p.ownText().startsWith("Promotion")) {
+                    promo = parseDetail(p);
+                }
             }
-            String name = studentContainer.getElementsByTag("h3").get(0).ownText().trim();
-            students.add(new StudentInfo(name, imageUrl));
+
+
+            students.add(new StudentInfo(name, imageUrl, birthday, email, promo, specialization));
         }
 
         return students;
     }
+
+    private String parseImageUrl(Element studentContainer) {
+        try {
+            String imageUrl = studentContainer.getElementsByClass("img-polaroid").get(0).attr("src");
+            if (imageUrl != null && imageUrl.startsWith("/")) {
+                imageUrl = Settings.Api.CAS_URL + imageUrl;
+            }
+            return imageUrl;
+        }
+        catch(Exception ex) {
+            return null;
+        }
+    }
+
+    private String parseName(Element studentContainer) {
+        try {
+            return studentContainer.getElementsByTag("h3").get(0).ownText().trim();
+        }
+        catch(Exception ex) {
+            return null;
+        }
+    }
+
+    private String parseDetail(Element p) {
+        try {
+            String ownText = p.ownText();
+            return p.ownText().split(":")[1].trim();
+        }
+        catch(Exception ex) {
+            return null;
+        }
+    }
+
+    private String parseEmail(Element p) {
+        try {
+            return p.getElementsByTag("a").get(0).ownText().trim();
+        }
+        catch(Exception ex) {
+            return null;
+        }
+    }
+
+
+
 
     public List<StudentInfo> getData() {
         return data;
--- app/src/main/java/fr/clementduployez/aurionexplorer/Model/StudentInfo.java
+++ app/src/main/java/fr/clementduployez/aurionexplorer/Model/StudentInfo.java
@@ -1,5 +1,7 @@
 package fr.clementduployez.aurionexplorer.Model;
 
+import java.util.Date;
+
 /**
  * Created by cdupl on 11/22/2016.
  */
@@ -10,10 +12,19 @@ public class StudentInfo {
     private String email;
     private String promo;
     private String name;
+    private String specialization;
 
     public StudentInfo(String name, String imageUrl) {
         this.name = name;
         this.imageUrl = imageUrl;
+    }
+
+    public StudentInfo(String name, String imageUrl, String birthday, String email, String promo, String specialization) {
+        this(name, imageUrl);
+        this.birthday = birthday;
+        this.email = email;
+        this.promo = promo;
+        this.specialization = specialization;
     }
 
     public String getImageUrl() {
